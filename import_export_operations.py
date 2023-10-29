eb25450372cbbe3ee6a780863971bb3a9fd58c44
benmor20 <benmor2020@gmail.com>
2023-10-28 20:03:56 -0400

    Imp/Exp with all 9 inputs

--- import_export_operations.py
+++ import_export_operations.py
@@ -41,18 +41,33 @@ GRID = [
     'JZWMRFLHQKDFTMUENPQBRFSBCJ'
 ]
 
+# The versions from the imports, in order
+IMPORTS = {
+    'ACTIVITY': 3,
+    'CHARACTER': 1,
+    'DISEASE': 3,
+    'HELLOWORLD': 1,
+    'MATH': 3,
+    'MODEL': 2,
+    'ROOM': 4,
+    'SETTING': 1,
+    'STATE': 5,
+}
+
 
 def main():
-    inp = int(input('Enter a number 0-8'))
-    x = ord(GUIDEWORD[inp]) - 65
-    y = ord(GUIDEWORD[inp + 1]) - 65
-    word = ''
-    while GRID[x][y] != 'X':
-        word += GRID[x][y]
-        temp = y
-        y = (x + 6) % 26
-        x = (temp + 14) % 26
-    print(word)
+    words = []
+    for inp in range(9):
+        x = ord(GUIDEWORD[inp]) - 65
+        y = ord(GUIDEWORD[inp + 1]) - 65
+        word = ''
+        while GRID[x][y] != 'X':
+            word += GRID[x][y]
+            temp = y
+            y = (x + 6) % 26
+            x = (temp + 14) % 26
+        print(f'{inp}: {word}')
+        words.append(word)
 
 # Be sure to consider the import version when evaluating export
 
